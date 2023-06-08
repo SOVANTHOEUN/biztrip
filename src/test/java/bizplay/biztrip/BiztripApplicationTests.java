41149dad15a912c8284128bf12945d407cbad435
SOVANTHOEUN <sovanbunthoeunvoung@gmail.com>
2023-06-08 14:37:19 +0900

    First application

---
+++ src/test/java/bizplay/biztrip/BiztripApplicationTests.java
@@ -0,0 +1,13 @@
+package bizplay.biztrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiztripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
