b2c353710fedaf858969584e58bc794a697b4871
SOVANTHOEUN <sovanbunthoeunvoung@gmail.com>
2023-06-08 14:47:01 +0900

    Develop

---
+++ src/main/java/bizplay/biztrip/BiztripApplication.java
@@ -0,0 +1,13 @@
+package bizplay.biztrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiztripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiztripApplication.class, args);
+    }
+
+}
